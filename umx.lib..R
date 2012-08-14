c0fc0031eba2afb0d0d63a1727cbd50311d6f393
tbates <timothy.c.bates@gmail.com>
2012-08-15 08:48:48 +1000

    initial commit

---
+++ umx.lib..R
@@ -0,0 +1,181 @@
+# umx.lib.R
+# To learn more, see
+# To USE ME IN YOUR SCRIPTS SAY: 
+# source("http://www.github.com/umx.lib.R")
+
+umxReportFit<-function(model) {
+	# use case
+	# umxReportFit(fit1)
+	with(summary(model),paste(
+		"χ2(", degreesOfFreedom, ") = ", round(Chi,2), ", ",
+		"p = "    , round(p,2)  , "; ",
+		"CFI = "  , round(CFI,3), "; ",
+		"TLI = "  , round(TLI,3), "; ",
+		"RMSEA = ", round(RMSEA, 3), sep="")
+	)
+}
+
+umxMI <- function(model, vector=T) {
+	# modification indices
+	# v0.9: written Michael Culbertson
+	# v0.91: up on github; added progress bar, Bates
+	# http://openmx.psyc.virginia.edu/thread/831
+	# http://openmx.psyc.virginia.edu/thread/1019
+	# http://openmx.psyc.virginia.edu/sites/default/files/mi-new.r
+	steps <- 5
+	bar <- txtProgressBar (min=0, max=steps, style=3)
+    setTxtProgressBar(bar, 1)
+	accumulate <- function(A, B, C, D, d) {
+		res <- matrix(0, d^2, d^2)    
+		for (ii in 1:(d^2)){
+			for (jj in ii:(d^2)){
+				g <- 1 + (ii - 1) %% d
+				h <- 1 + (ii - 1) %/% d
+				i <- 1 + (jj - 1) %% d
+				j <- 1 + (jj - 1) %/% d
+				res[ii, jj] <- res[jj, ii] <- A[g, i] * B[h, j] + C[g, j] * D[h, i]
+			}
+		}
+		res
+	}
+	accumulate.asym <- function(A, B, C, D, d) {
+		res <- matrix(0, d^2, d^2)    
+		for (ii in 1:(d^2)){
+			for (jj in 1:(d^2)){
+				g <- 1 + (ii - 1) %% d
+				h <- 1 + (ii - 1) %/% d
+				i <- 1 + (jj - 1) %% d
+				j <- 1 + (jj - 1) %/% d
+				res[ii, jj] <- A[g, i] * B[h, j] + C[g, j] * D[h, i]
+			}
+		}
+		res
+	}
+	A <- model$A@values
+	P <- model$S@values
+	S <- model$data@observed
+	J <- model$F@values
+	m <- dim(A)[1]
+	which.free <- c(model$A@free, model$S@free & upper.tri(diag(m), diag=T))
+	vars       <- colnames(A)
+	parNames   <- c(model$A@labels, model$S@labels)
+	parNames[is.na(parNames)] <- c(outer(vars, vars, paste, sep=' <- '),
+	outer(vars, vars, paste, sep=' <-> '))[is.na(parNames)]
+	NM     <- model$data@numObs - 1
+	I.Ainv <- solve(diag(m) - A) 
+	C      <- J %*% I.Ainv %*% P %*% t(I.Ainv) %*% t(J)
+	Cinv   <- solve(C)    
+	AA     <- t(I.Ainv) %*% t(J)
+	BB     <- J %*% I.Ainv %*% P %*% t(I.Ainv)
+	correct <- matrix(2, m, m)
+	diag(correct) <- 1
+	grad.P <- correct * AA %*% Cinv %*% (C - S) %*% Cinv %*% t(AA)
+	grad.A <- correct * AA %*% Cinv %*% (C - S) %*% Cinv %*% BB 
+	grad   <- c(grad.A, grad.P) * NM
+	names(grad) <- parNames
+	dF.dBdB <- accumulate(AA %*% Cinv %*% t(AA), t(BB) %*% Cinv %*% BB, AA %*% Cinv %*% BB, t(BB) %*% Cinv %*% t(AA), m)
+    setTxtProgressBar(bar, 2)
+	dF.dPdP <- accumulate(AA %*% Cinv %*% t(AA), AA %*% Cinv %*% t(AA), AA %*% Cinv %*% t(AA), AA %*% Cinv %*% t(AA), m)
+    setTxtProgressBar(bar, 3)
+	dF.dBdP <- accumulate.asym(AA %*% Cinv %*% t(AA), t(BB) %*% Cinv %*% t(AA), AA %*% Cinv %*% t(AA), t(BB) %*% Cinv %*% t(AA), m)
+    setTxtProgressBar(bar, 4)
+	correct.BB <- correct.PP <- correct.BP <- matrix(1, m^2, m^2)
+	correct.BB[diag(m)==0, diag(m)==0] <- 2
+	correct.PP[diag(m)==1, diag(m)==1] <- 0.5
+	correct.PP[diag(m)==0, diag(m)==0] <- 2
+	correct.BP[diag(m)==0, diag(m)==0] <- 2
+	Hessian <- NM*rbind(cbind(dF.dBdB * correct.BB,    dF.dBdP * correct.BP),
+	cbind(t(dF.dBdP * correct.BP), dF.dPdP * correct.PP))
+	rownames(Hessian) <- parNames
+	colnames(Hessian) <- parNames
+	# list(gradient=grad[which.free], Hessian[which.free, which.free])
+
+	hessian <- Hessian[which.free, which.free]
+	E.inv <- solve(hessian)
+	par.change <- mod.indices <- rep(0, 2*(m^2))                
+	for (i in 1:(2*(m^2))) {
+		k <- Hessian[i, i]
+		d <- Hessian[i, which.free]
+		par.change[i]  <- (-grad[i] / (k - d %*% E.inv %*% d))
+		mod.indices[i] <- (-0.5 * grad[i] * par.change[i])
+	}
+	names(mod.indices) <- parNames
+	names(par.change)  <- parNames
+	if (vector) {
+		which.ret <- c(!model$A@free & !diag(m), !model$S@free) # & upper.tri(diag(m), diag=T))
+		sel <- order(mod.indices[which.ret], decreasing=T)
+		ret <- list(mi=mod.indices[which.ret][sel], par.change=par.change[which.ret][sel])
+	} else {
+		mod.A <- matrix(mod.indices[1:(m^2)]   , m, m)
+		mod.P <- matrix(mod.indices[-(1:(m^2))], m, m)
+		par.A <- matrix(par.change[1:(m^2)]    , m, m)
+		par.P <- matrix(par.change[-(1:(m^2))] , m, m)
+		rownames(mod.A) <- colnames(mod.A) <- vars
+		rownames(mod.P) <- colnames(mod.P) <- vars
+		rownames(par.A) <- colnames(par.A) <- vars
+		rownames(par.P) <- colnames(par.P) <- vars
+		mod.A[model$A@free] <- NA
+		par.A[model$A@free] <- NA
+		diag(mod.A) <- NA
+		diag(par.A) <- NA
+		mod.P[model$S@free] <- NA
+		par.P[model$S@free] <- NA
+		ret <- list(mod.A=mod.A, par.A=par.A, mod.S=mod.P, par.S=par.P)
+	}
+    setTxtProgressBar(bar, 5)
+	close(bar)
+	return(ret)
+}
+
+umxMI_top <- function(fit=NA, numInd=10, typeToShow="both") {
+	# depends on umxMI(fit)
+	# use cases
+	# mi.df = umxMI_top(fit)
+	# umxMI_top(fit, numInd=5, typeToShow="add") # valid options are "both|add|delete"
+	mi = umxMI(fit, vector=T)
+	mi.df = data.frame(path= as.character(attributes(mi$mi)$names), value=mi$mi);
+	row.names(mi.df) = 1:nrow(mi.df);
+	# TODO: could be a helper: choose direction
+	mi.df$from = sub(pattern="(.*) +(<->|<-|->) +(.*)", replacement="\\1", mi.df$path)
+	mi.df$to   = sub(pattern="(.*) +(<->|<-|->) +(.*)", replacement="\\3", mi.df$path)
+	mi.df$arrows = 1
+	mi.df$arrows[grepl("<->", mi.df$path)]= 2		
+
+	mi.df$action = NA 
+	mi.df  = mi.df[order(mi.df[,2], decreasing=T),] 
+	mi.df$copy = 1:nrow(mi.df)
+	for(n in 1:(nrow(mi.df)-1)) {
+		if(grepl(" <- ", mi.df$path[n])){
+			tmp = mi.df$from[n]; mi.df$from[n] = mi.df$to[n]; mi.df$to[n] = tmp 
+		}
+		from = mi.df$from[n]
+		to   = mi.df$to[n]
+		a = (fit@matrices$S@free[to,from] |fit@matrices$A@free[to,from])
+		b = (fit@matrices$S@values[to,from]!=0 |fit@matrices$A@values[to,from] !=0)
+		if(a|b){
+			mi.df$action[n]="delete"
+		} else {
+			mi.df$action[n]="add"
+		}
+		inc= min(4,nrow(mi.df)-(n))
+		for (i in 1:inc) {
+			if((mi.df$copy[(n)])!=n){
+				# already dirty
+			}else{
+				# could be a helper: swap two 
+				from1 = mi.df[n,"from"]     ; to1   = mi.df[n,"to"]
+				from2 = mi.df[(n+i),"from"] ; to2   = mi.df[(n+i),'to']
+				if((from1==from2 & to1==to2) | (from1==to2 & to1==from2)){
+					mi.df$copy[(n+i)]<-n
+				}
+			}			
+		}
+	}
+	mi.df = mi.df[unique(mi.df$copy),] # c("copy")
+	if(typeToShow!="both"){
+		mi.df = mi.df[mi.df$action==typeToShow,]
+	}
+	print(mi.df[1:numInd, !(names(mi.df) %in% c("path","copy"))])
+	invisible(mi.df)
+}
+
