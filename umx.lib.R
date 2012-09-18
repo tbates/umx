a4607ca401c468a449ac5afc3339039b8d8b22d3
tim bates <timothy.c.bates@gmail.com>
2012-09-18 17:07:18 +0100

    feedback enahncement to umxUpdateOpenMx

--- umx.lib.R
+++ umx.lib.R
@@ -12,11 +12,12 @@ script <- RCurl::getURL(url, ssl.verifypeer = FALSE)
 #` parse then evaluate script in the global environement
 eval(parse(text = script))
 #` Code borrowed from [here](http://tonybreyal.wordpress.com/2011/11/24/source_https-sourcing-an-r-script-from-github)
-
 umxUpdateOpenMx <-function(bleedingEdge=FALSE, loadNew=TRUE) {
 	# update the OpenMx Library to latest version:
 	# umxUpdateOpenMx()
 	if( "OpenMx" %in% .packages() ){
+		oldV = mxVersion();
+		message("existing version \"" ,oldV, "\" was detached")
 		detach(package:OpenMx); # unload existing version
 	}	
 	if (bleedingEdge){
@@ -84,8 +85,12 @@ umxUpdateOpenMx <-function(bleedingEdge=FALSE, loadNew=TRUE) {
 		}
 	}
 	if(loadNew){
-		require("OpenMx")	
+		require("OpenMx")
+		newV = mxVersion();
+		message("Woot: you have upgraded from version \"" ,oldV, "\" to the latest and greatest \"", newV, "\"!")
+		detach(package:OpenMx); # unload existing version
 	}
+
 }
 
 
