93949a43c4e36c29c5edff6b1904befb0f26bd21
tbates <timothy.c.bates@gmail.com>
2013-04-04 22:14:53 +0100

    utility function for rename files on disk added...

--- umx.lib.R
+++ umx.lib.R
@@ -1861,3 +1861,43 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     }
   }
 }
+
+renameFile <- function(baseFolder = "Finder", findStr=NA, replaceStr=NA, listPattern = NA, test=T, overwrite=F) {
+	# renameFile(baseFolder = "~/Downloads/", findStr="", replaceStr="", listPattern = "", test=T)
+	# renameFile(baseFolder = NA, findStr="", replaceStr="", listPattern = "", test=T)
+	# uppercase = u$1
+	if(baseFolder=="Finder"){
+		baseFolder = system(intern=T, "osascript -e 'tell application \"Finder\" to get the POSIX path of (target of front window as alias)'")
+		message("Using front-most Finder window:", baseFolder)
+	} else if(baseFolder == "") {
+		baseFolder = paste(dirname(file.choose(new = FALSE)), "/", sep="") ## choose a directory
+		message("Using selected folder:", baseFolder)
+	}
+	if(is.na(listPattern)){
+		listPattern= findStr
+	}
+	a = list.files(baseFolder, pattern=listPattern)
+	message("found ", length(a), " possible files")
+	changed = 0
+	for (fn in a) {
+		findB = grepl(pattern=findStr, fn) #returns 1 if found
+		if(findB){
+			fnew = gsub(findStr, replace = replaceStr, fn) # replace all instances
+			if(test){
+				message("would change ", fn, " to ", fnew)	
+			} else {
+				if((!overwrite) & file.exists(paste(baseFolder, fnew, sep=""))){
+					message("renaming ", fn, "to", fnew, "failed as already exists. To overwrite set T")
+				} else {
+					file.rename(paste(baseFolder, fn, sep=""), paste(baseFolder, fnew, sep=""))
+					changed = changed+1;
+				}
+			}
+		}else{
+			if(test){
+				# message(paste("bad file",fn))
+			}
+		}
+	}
+	message("changed ", changed)
+}
