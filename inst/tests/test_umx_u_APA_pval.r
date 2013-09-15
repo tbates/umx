b823b84feef8b9c61dc05128b85d8f40057b4830
tim bates <timothy.c.bates@gmail.com>
2013-09-15 15:56:38 +0100

    my first test!

--- inst/tests/test_umx_u_APA_pval.r
+++ inst/tests/test_umx_u_APA_pval.r
@@ -1,7 +1,6 @@
-# test_that("umx_u_APA_pval works for different values", {
-# 	expect_that(umx_u_APA_pval(1.23E-3), "= 0.001")
-# 	expect_that(umx_u_APA_pval(c(1.23E-3, .5)), c("= 0.001", "= 0.5"))
-# 	expect_that(umx_u_APA_pval(c(1.23E-3, .5), addComparison = F), c(0.001, 0.500))
-# 	expect_that(, "= 0.001")
-# 	expect_that(, "= 0.001")
-# })
+test_that("umx_u_APA_pval works for different values", {
+	expect_that(umx_u_APA_pval(p = .04, min = .05), matches("< 0.05") )
+	expect_that(umx_u_APA_pval(1.23E-3)           , matches("= 0.001") )
+	expect_that(umx_u_APA_pval(c(1.23E-3, .5))    , matches( c("= 0.001", "= 0.5") ) )
+	expect_that(umx_u_APA_pval(c(1.23E-3, .5), addComparison = F), equals(c(0.001, 0.500)) )
+})
