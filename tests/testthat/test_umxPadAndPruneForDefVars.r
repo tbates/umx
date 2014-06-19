3f72d90fdf80a2909be4e0b9175e1590ded977de
tbates <timothy.c.bates@gmail.com>
2014-06-19 17:00:03 -0400

    don't step on data. labels !

---
+++ tests/testthat/test_umxPadAndPruneForDefVars.r
@@ -0,0 +1,24 @@
+# library(testthat); library(devtools)
+# test_file("~/bin/umx/tests/testthat/test_umxPadAndPruneForDefVars.r")
+# 
+# test_package("umx")
+	require(umx)
+x1 = c(1:3,NA,5:10)
+x2 = c(1:3,NA,5:10)
+x3 = c(1:3,NA,5:9, NA)
+d1 = c(1:3,NA,5:9, NA)
+d2 = c(1:3,NA,5:9, NA)*3
+d3 = c(1:3,NA,5:9, NA)*10
+
+df = data.frame(x1, x2, x3, d1, d2, d3)
+defVars = c("d1", "d2", "d3")
+vars    = c("x1", "x2", "x3")
+df
+testEF = df
+testEF[4 , defVars] = 99
+testEF[10, defVars] = c(5.125, 15.375, 51.25)
+ef = umxPadAndPruneForDefVars(df, vars, defVars, highDefValue= 99, suffixes = c(""))
+
+test_that("umxPadAndPruneForDefVars works", {	
+	testthat::expect_equivalent(ef, testEF)
+})
