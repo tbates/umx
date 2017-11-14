0a6f84c99217c9c00327cabbfe027856863e4e08
Timothy Bates <timothy.c.bates@gmail.com>
2017-11-14 20:47:30 +0000

    mikey mods on tmx gene effect simulator.

--- R/tmx.r
+++ R/tmx.r
@@ -13,8 +13,10 @@
 #' The d and h increments of the gene difference A – a. Aa may lie on either side of m and the sign of h will
 #' vary accordingly; in the case illustrated h would be negative. (Adapted from Mather and Jinks, 1977, p. 32).
 #' See book issue 23
-#' This function lets you explore the simplest two–allele system (A and a), with three possible genotypes, AA, Aa, and aa.
-#' Parameter "a" is half the measured difference between the homozygotes AA and aa. The point between the two homozygotes is "m" -- the mean effect of homozygous genotypes. "d" defines the measured deviation effect of the heterozygote Aa, relative to this expected midpoint. "a" and "d" are genotypic effects.
+#' This function lets you explore the simplest two–allele system (B and b), with three possible genotypes, BB, Bb, and bb.
+#' Parameter "b" is half the measured difference between the homozygotes BB and bb. The point between the two homozygotes 
+#' is "m" -- the mean effect of homozygous genotypes. "d" defines the measured deviation effect of the heterozygote Aa, 
+#' relative to this expected midpoint. "a" and "d" are genotypic effects.
 #' 
 #' old system:
 #' 
@@ -52,12 +54,12 @@
 #' # ggsave(paste0(base, "c03_genotypic_effect_by_gene_dose.pdf"), width = 4.6, height = 4.6)
 tmx_genotypic_effect <- function(p = .75, q = .25, a = .5, d = .25, m = 0, show = TRUE){
 	require(ggplot2)
-	# Genotypes AA Aa aa Frequency p2 2pq q2
+	# Genotypes BB Bb bb Frequency p2 2pq q2
 	df <- data.frame(stringsAsFactors = FALSE,
 		Genotypes = c("Frequency", "fraction"),
-		AA = c("p\u00B2", round(p^2, 2)),
-		Aa = c("2pq"    , round(2 * p * q, 2)),
-		aa = c("q\u00B2", round(q^2, 2))
+		BB = c("p\u00B2", round(p^2, 2)),
+		Bb = c("2pq"    , round(2 * p * q, 2)),
+		bb = c("q\u00B2", round(q^2, 2))
 	)
 	message("Genotype frequencies at Hardy Weinberg Equilibrium (p= ", p," q = ", q, ")")
 	umx_print(df)
@@ -67,20 +69,20 @@ tmx_genotypic_effect <- function(p = .75, q = .25, a = .5, d = .25, m = 0, show 
 	# ===========================================================
 	# p  = .5; q = .5; a = 1; d = .5; m = 0
 	# genotypic values section of pink book
-	# G_AA_A = additive component of Genotypic value of AA
-	G_AA_A = (2 * q) * (a + d * (q - p));
-	G_AA_D =  2 * q^2 * d; 
-	G_Aa_A = (q - p) * (a + d * (q - p));
-	G_Aa_D =  2 * p*q * d; 
-	G_aa_A = (-2* p) * (a + d * (q - p));
-	G_aa_D =  2 * p^2 * d; 
+	# G_BB_A = additive component of Genotypic value of BB
+	G_BB_A = (2 * q) * (a + d * (q - p));
+	G_BB_D =  2 * q^2 * d; 
+	G_Bb_A = (q - p) * (a + d * (q - p));
+	G_Bb_D =  2 * p*q * d; 
+	G_bb_A = (-2* p) * (a + d * (q - p));
+	G_bb_D =  2 * p^2 * d; 
 	
-	G_aa   = (G_aa_A - G_aa_D);
-	G_Aa   = (G_Aa_A + G_Aa_D);
-	G_AA   = (G_AA_A - G_AA_D);
-	G_aa_f = q^2
-	G_Aa_f = 2 * p * q
-	G_AA_f = p^2
+	G_bb   = (G_bb_A - G_bb_D);
+	G_Bb   = (G_Bb_A + G_Bb_D);
+	G_BB   = (G_BB_A - G_BB_D);
+	G_bb_f = q^2
+	G_Bb_f = 2 * p * q
+	G_BB_f = p^2
 	
 	# mu = 2 * p^2 + 2 * p * q
 	# meanDose = 2 * p^2 + 2 * p * q  # eq 3.14
@@ -88,10 +90,10 @@ tmx_genotypic_effect <- function(p = .75, q = .25, a = .5, d = .25, m = 0, show 
 	# df$effect * df$freq; sum(df$effect * df$freq) # sums to zero
 	# TODO add examples on page 57
 	df <- data.frame(stringsAsFactors = FALSE,
-		dose     = c(0     , 1     , 2    ),
-		genotype = c("aa"  , "Aa"  , "AA" ),
-		value    = c(G_aa  , G_Aa  , G_AA  ),
-		freq     = factor(c(G_aa_f, G_Aa_f, G_AA_f))
+		dose     = c(0            , 1     , 2      ),
+		genotype = c("bb"         , "Bb"  , "BB"   ),
+		value    = c(G_bb         , G_Bb  , G_BB   ),
+		freq     = factor(c(G_bb_f, G_Bb_f, G_BB_f))
 	)
 	# Plot regression line, and points (sized to frequency)
 	thePlot = qplot(x = dose, y = value, geom = "point", size = freq, xlab = "Gene Dose", ylab = "Genotypic VALUE", data = df)
@@ -103,10 +105,10 @@ tmx_genotypic_effect <- function(p = .75, q = .25, a = .5, d = .25, m = 0, show 
 	# ================================================
 	# p55, pink book, fig 3.2
 	df <- data.frame(stringsAsFactors = FALSE,
-		dose     = c(0    , 1    , 2    ),
-		genotype = c("aa" , "Aa" , "AA" ),
+		dose     = c(0     , 1    , 2   ),
+		genotype = c("bb"  , "Bb" , "BB"),
 		effect   = c(-a    , d    , a   ),
-		freq     = c(G_aa_f, G_Aa_f, G_AA_f)
+		freq     = c(G_bb_f, G_Bb_f, G_BB_f)
 	)
 	message("Genotypic Effects")
 	umx_print(df)
@@ -122,7 +124,8 @@ tmx_genotypic_effect <- function(p = .75, q = .25, a = .5, d = .25, m = 0, show 
 	thePlot = thePlot + scale_x_continuous(breaks = c(0, 1, 2))
 	
 	# 2. Add labels to plot, in the data coordinates.
-	thePlot = thePlot + geom_point(aes(x = 1, y=m), color="red") + cowplot::draw_label("m", x = 1+.1, y = m, fontfamily="Optima")
+	# don't draw m, misleading
+	# thePlot = thePlot + geom_point(aes(x = 1, y=m), color="red") + cowplot::draw_label("m", x = 1+.1, y = m, fontfamily="Optima")
 
 	# set the y axis
 	# TODO: lave numbers on y axis, add text labels beside these?
@@ -132,13 +135,28 @@ tmx_genotypic_effect <- function(p = .75, q = .25, a = .5, d = .25, m = 0, show 
 		thePlot = thePlot + scale_y_continuous(breaks = c(-a, m, d, a), labels = c("-a", "m", "d", "+a"))
 	}
 	# TODO: plot points on the line, add text labels beside these
-	thePlot = thePlot + geom_point(aes(x = 0, y= -a), color="red") + cowplot::draw_label("aa", x = 0+.1, y = -a, fontfamily="Optima")
-	thePlot = thePlot + geom_point(aes(x = 1, y=  d), color="red") + cowplot::draw_label("Aa", x = 1-.1, y =  d, fontfamily="Optima")
-	thePlot = thePlot + geom_point(aes(x = 2, y=  a), color="red") + cowplot::draw_label("AA", x = 2, y =  a-.1, fontfamily="Optima")
+	addArrow <- function(thePlot, x1, b, d, m, y2) {
+		segs <- data.frame(
+			x1 = x1,
+			y1 = (x1 - 1) * b + .5 * d + m,
+			y2 = y2
+		)
+		# thePlot + geom_segment(aes(x = x1, y = y1, xend = x1, yend = y2), arrow = arrow(length=unit(0.30,"cm")), data = segs)
+		thePlot + geom_segment(aes(x = x1, y = y1, xend = x1, yend = y2), arrow = arrow(length=unit(0.10,"cm")), data = segs)
+	}
+	# bb
+	thePlot = thePlot + geom_point(aes(x = 0, y = -a), color = "black") 
+	thePlot = thePlot + cowplot::draw_label("bb", x = 0+.1, y = -a, fontfamily = "Optima")
+	thePlot = addArrow(thePlot, x1 = 0, b=b, d=d, m=m, y2=-a)
+	# Bb
+	thePlot = thePlot + geom_point(aes(x = 1, y= d), color = "black") 
+	thePlot = thePlot + cowplot::draw_label("Bb", x = (1-.1), y =  d, fontfamily = "Optima")
+	thePlot = addArrow(thePlot, x1 = 1, b=b, d=d, m=m, y2=d)
+	# BB
+	thePlot = thePlot + geom_point(aes(x = 2, y= a), color = "black") 
+	thePlot = thePlot + cowplot::draw_label("BB", x = 2, y =  (a -.1), fontfamily = "Optima")
+	thePlot = addArrow(thePlot, x1 = 2, b = b, d = d, m = m, y2 = a)
 
-	# thePlot = thePlot + cowplot::draw_label("aa", x = 0, y = -a, fontfamily="Optima")
-	# thePlot = thePlot + cowplot::draw_label("Aa", x = 1, y =  d, fontfamily="Optima")
-	# thePlot = thePlot + cowplot::draw_label("AA", x = 2, y =  a, fontfamily="Optima")
 	thePlot = thePlot + expand_limits(y = c(-a, a))
 	if(show){
 		print(thePlot)
