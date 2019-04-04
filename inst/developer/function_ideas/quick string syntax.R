129e888fd5dc083709dc061a7419d4f9813aa14a
Timothy Bates <timothy.c.bates@gmail.com>
2019-04-04 11:23:29 +0100

    no message

--- inst/developer/function_ideas/quick string syntax.R
+++ inst/developer/function_ideas/quick string syntax.R
@@ -1,33 +1,33 @@
-HS.model <- '  visual =~ x1 + x2 + x3
+lav <- '  visual =~ x1 + x2 + x3
               textual =~ x4 + x5 + x6
               speed   =~ x7 + x8 + x9 '
 
-fit <- cfa(HS.model, 
-           data = HolzingerSwineford1939, 
-           group = "school")
+lavm <- cfa(lav, data = HolzingerSwineford1939, group = "school")
+summary(lavm)
 
-summary(fit)
-
-To convert lavaan to OpenMx
-1. replace "=~" with "->"
-2. add the black-box elements "visual <-> visual"
-HS.model <- " visual  -> x1@1 + x2 + x3
-              textual -> x4@1 + x5 + x6
-              speed   -> x7@1 + x8 + x9
-			  # Added silently by lavaan (also the @1 above)
-			  visual <-> visual 
-			  textual<-> textual
-			  speed  <-> speed
-			  x1     <-> x1
-			  x2     <-> x2
-			  x3     <-> x3
-			  x4     <-> x4
-			  x5     <-> x5
-			  x6     <-> x6
-              x7     <-> x7
-			  x8     <-> x8
- 			  x9     <-> x9
+# To convert lavaan to OpenMx
+# 1. replace "=~" with "->"
+# 2. add the black-box elements "visual <-> visual"
+HS.model <- "
+	visual  -> x1@1 + x2 + x3
+	textual -> x4@1 + x5 + x6
+	speed   -> x7@1 + x8 + x9
+	
+	# Added silently by lavaan (also the @1 above)
+	visual <-> visual 
+	textual<-> textual
+	speed  <-> speed
+	   x1  <-> x1
+	   x2  <-> x2
+	   x3  <-> x3
+	   x4  <-> x4
+	   x5  <-> x5
+	   x6  <-> x6
+	   x7  <-> x7
+	   x8  <-> x8
+	   x9  <-> x9
 "
+
 fit <- cfa(HS.model, 
            data = HolzingerSwineford1939, 
            group = "school")
