2df74d3c8c06efec055b09438be5c7e4a526db88
Timothy Bates <timothy.c.bates@gmail.com>
2019-09-26 14:52:52 +0100

    * IMPROVED: `umxRAM` now respects `umx_set_silent`

--- R/build_run_modify.R
+++ R/build_run_modify.R
@@ -614,16 +614,16 @@ umxRAM <- function(model = NA, ..., data = NULL, name = NA, group = NULL, group.
 	latentVars = setdiff(foundNames, c(manifestVars, "one"))
 	nLatent = length(latentVars)
 	# Report which latents were created
-  if (verbose) {
-    if(nLatent == 0){
-      # message("No latent variables were created.\n")
-      # latentVars = NA
-    } else if (nLatent == 1){
-      message("A latent variable '", latentVars[1], "' was created. ")
-    } else {
-      message(nLatent, " latent variables were created:", paste(latentVars, collapse = ", "), ". ")
-    }
-  }
+	if (!umx_set_silent(silent=TRUE)) {
+    	if(nLatent == 0){
+			# message("No latent variables were created.\n")
+			# latentVars = NA
+	    } else if (nLatent == 1){
+			message("A latent variable '", latentVars[1], "' was created. ")
+	    } else {
+      	  message(nLatent, " latent variables were created:", paste(latentVars, collapse = ", "), ". ")
+    	}
+	}
 
 	# ===========================================================
 	# = TODO handle user adding an mxThreshold object to umxRAM =
