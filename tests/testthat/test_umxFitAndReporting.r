bf945d7fe6e21f4873ba3f4d181c7b1c67f4c10b
Tim Bates <timothy.c.bates@gmail.com>
2021-12-08 12:27:55 +0000

    Create test_umxFitAndReporting.r

---
+++ tests/testthat/test_umxFitAndReporting.r
@@ -0,0 +1,20 @@
+# library(testthat)
+# library(umx)
+# test_file("~/bin/umx/tests/testthat/test_umxFitAndReporting.r") 
+# test_package("umx")
+
+context("umxFitAndReporting_Functions")
+
+test_that("umxDiagnose works", {
+	require(umx)
+	data(demoOneFactor)
+	manifests = names(demoOneFactor)
+
+	m1 = umxRAM("One Factor", data = demoOneFactor, type = "cov",
+		umxPath("G", to = manifests),
+		umxPath(var = manifests),
+		umxPath(var = "G", fixedAt = 1)
+	)
+
+	umxDiagnose(m1)
+})
