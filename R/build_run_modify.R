48d2472c611c4c1eb0b2c5956c79e92ae6f8e7b3
Timothy Bates <timothy.c.bates@gmail.com>
2023-05-08 16:43:14 +0100

    Update build_run_modify.R

--- R/build_run_modify.R
+++ R/build_run_modify.R
@@ -12,6 +12,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+# An institution is the lengthened shadow of one man. Emerson.
+
 # =======================
 # = Markdown hyperlinks =
 # =======================
