e2312d270beb3eb35bf08507f6190cbbee1f4f98
Tim Bates <timothy.c.bates@gmail.com>
2026-07-03 14:42:26 +1200

    Update test_WLS_lavaan.R

--- tests/testthat/test_WLS_lavaan.R
+++ tests/testthat/test_WLS_lavaan.R
@@ -6,7 +6,7 @@ library(testthat)
 test_that("WLS in lavaan and umxRAM identical", {
 	# 1. Generate the Data 
 	set.seed(12345)
-	n  = 800
+	n  = 100
 	f  = rnorm(n)
 	x1 = 0.75 * f + rnorm(n, sd = 0.66)
 	x2 = 0.80 * f + rnorm(n, sd = 0.60)
@@ -24,7 +24,7 @@ test_that("WLS in lavaan and umxRAM identical", {
 	# TODO: implement m.v. as a synonym for v.m.?
 	m1 = umxRAM("WLS_umx", data = dat, type = "WLS", allContinuousMethod = "cumulants",   # or "marginals"
     	umxPath("f", to = c("x1", "x2", "x3", "x4") ),
-		umxPath(v.m. = "f"),
+		umxPath(v1m0 = "f"),
 		umxPath(v.m. = c("x1", "x2", "x3", "x4"))
 	)
 
@@ -32,28 +32,22 @@ test_that("WLS in lavaan and umxRAM identical", {
 	model = ' f =~ x1 + x2 + x3 + x4 '
 	lav1  = cfa(model, data = dat, ordered = names(dat), estimator = "WLSMV")
 
+	# TODO compare results!
 	
 	# 3. Fit the Nested WLS Model (Drop the b1 path to 0)
-	mNested = mxModel(mBase, name = "WLS_Nested")
-	mNested$A$values["y", "x"] = 0
-	mNested$A$free["y", "x"] = FALSE
-	mNested = mxRun(mNested, silent = TRUE)
+	m2 umxModify(m1, name = "WLS_Nested")
 
-  # 4. Fit an ML Model (Non-WLS)
-  mMl = mxModel("ML_Base", type="RAM",
-    manifestVars = c("x", "y"),
-    mxPath(from = "x", to = "y", arrows = 1, free = TRUE, values = 0.2, labels = "b1"),
-    mxPath(from = c("x", "y"), arrows = 2, free = TRUE, values = 1),
-    mxPath(from = "one", to = c("x", "y"), free = TRUE),
-    mxData(observed = df, type = "raw")
-  )
-  mMl = mxRun(mMl, silent = TRUE)
+	# 4. Fit an ML Model (Non-WLS)
+	m3 = umxRAM("ML_Base", mxData(observed = df, type = "raw"),
+		umxPath(from = "x", to = "y"),
+		umxPath("x", with= "y"),
+		umxPath(v.m.= c("x", "y")),
+		
+	)
 
-  # 5. Verify umxCompare routes to xmu_compare_WLS
-  res_direct = xmu_compare_WLS(mBase, mNested)
-  res_routed = umxCompare(mBase, mNested)
-  expect_equal(res_routed, res_direct)
+	# 5. Verify umxCompare routes to xmu_compare_WLS
+	res_direct = xmu_compare_WLS(m1, m2)
+	res_routed = umxCompare(m1, m2)
+	expect_equal(res_routed, res_direct)
 
-  # 6. Verify error on mixing WLS and ML
-  expect_error(umxCompare(mBase, mMl), "Cannot compare a WLS model with an ML model.")
 })
